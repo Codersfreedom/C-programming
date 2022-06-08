345d2f0547d797da649c5a00b92415eb5e3c6d3f
NARUTO <102137435+Codersfreedom@users.noreply.github.com>
2022-06-08 22:01:57 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -1,60 +1,71 @@
-#include <stdio.h>
-int a[20][20], q[20], visited[20], n, i, j, f = -1, r = -1, traverse[100], k = 1;
+#include<stdio.h>
+#include<stdlib.h>
 
-void bfs(int v)
-{
+int ar[20][20];
+int q[20],front,rear;
 
-    for (i = 1; i <= n; i++)
-        if (a[v][i] && !visited[i])
-        {
-            q[++r] = i;
-            if (f == -1)
-                f++;
-        }
-    if (f <= r)
-    {
-        visited[q[f]] = 1;
-        traverse[k++] = q[f];
-        bfs(q[f++]);
-    }
+void enQ(int num){
+	if(front==-1 && rear==-1)
+		front=rear=0;
+	else rear++;
+	q[rear]=num;
 }
 
-void main()
-{
-    int v;
-    printf("\n Enter the number of vertices:");
-    scanf("%d", &n);
-
-    for (i = 1; i <= n; i++)
-    {
-        q[i] = 0;
-        visited[i] = 0;
-    }
-
-    printf("\n Enter graph data in matrix form:\n");
-    for (i = 1; i <= n; i++)
-    {
-        for (j = 1; j <= n; j++)
-        {
-            scanf("%d", &a[i][j]);
-        }
-    }
-
-    printf("\n Enter the starting vertex:");
-    scanf("%d", &v);
-    visited[v] = 1;
-    traverse[k++] = v;
-    bfs(v);
-    printf("\n The node which are reachable are:\n");
-
-    for (i = 1; i <= n; i++)
-    {
-        if (visited[traverse[i]])
-            printf("%d\t", traverse[i]);
-        else
-        {
-            printf("\n Bfs is not possible. Not all nodes are reachable");
-            break;
-        }
-    }
+int deQ(){
+	int index;
+	if(front==rear && rear==-1)
+		return -1;
+	else{
+		index=front;
+		front++;
+		if(rear<front)
+			rear=front=-1;
+		return q[index];
+	}
 }
+
+void traverse(int n,int src){
+	int del,i;
+	int *p=(int*)calloc(n,sizeof(p));
+	enQ(src); p[src]=1;
+	for(;front!=-1;){
+		del=deQ();
+		printf("%d ",del);
+		for(i=0;i<n;i++){
+			if(ar[del][i]==1 && p[i]==0){
+				enQ(i);
+				p[i]=1;
+			}
+		}
+	}
+}
+
+int main(){
+	int i=20,j,n,t,a,b,src;
+	front=rear=-1;
+	while(i--) for(j=0;j<20;j++) ar[i][j]=0;
+	printf("Enter number of Vertices: ");
+	scanf("%d",&n);
+	printf("Enter number of Edges: ");
+	scanf("%d",&t);
+	printf("Enter the Edges:\n");
+	while(t--){
+		scanf("%d",&a);scanf("%d",&b);
+		ar[a][b]=ar[b][a]=1;
+	}
+	printf("Enter the Source Vertex: ");
+	scanf("%d",&src);
+	printf("The B.F.S. traversal is: ");
+	traverse(n,src);
+	return 0;
+}
+/*
+6
+5
+0 1
+1 2
+0 2
+3 4
+4 4
+
+*/
